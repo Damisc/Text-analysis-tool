b11b3d217880cade42a8769829a33c921643ce2e
Damisc <dammyoladapo2021@gmail.com>
2025-12-15 17:09:31 +0100

    Added user input validation

--- analysis.py
+++ analysis.py
@@ -1,3 +1,6 @@
+from random_username.generate import generate_username
+
+
 
 def welcome_user():
     # Welcome user
@@ -6,6 +9,12 @@ def welcome_user():
 def get_username():
     # print message prompting user to input their name 
     usernameFromInput = input("To begin, Please enter your username: ")
+
+    if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
+        print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), no spaces, not begin with numbers.")
+        print("Assigning new username instead ....")
+        return generate_username()[0]
+
     return usernameFromInput
 
 def greet_user(name):
