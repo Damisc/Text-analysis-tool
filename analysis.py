77f59a7ba9041c2d93a485d1bb11b21a7cb80600
Damisc <dammyoladapo2021@gmail.com>
2025-12-28 19:07:47 +0100

    Gave user 3 attempts to enter a correct username

--- analysis.py
+++ analysis.py
@@ -7,15 +7,29 @@ def welcome_user():
     print("Welcome to the text analysis tool, i will mine and analyze a body of text from a file you give me")
 
 def get_username():
-    # print message prompting user to input their name 
-    usernameFromInput = input("To begin, Please enter your username: ")
 
-    if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
-        print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), no spaces, not begin with numbers.")
-        print("Assigning new username instead ....")
-        return generate_username()[0]
+    attempts = 0
+    max_attempts = 3
+    while attempts < max_attempts:
 
-    return usernameFromInput
+        # print message prompting user to input their name 
+        UserInput = " "
+        if attempts == 0:
+            UserInput = "\nTo begin, Please enter your username:\n"
+        else:
+            UserInput = "\nPlease try again: \n"
+        usernameFromInput = input(UserInput)
+
+        # Validate username
+        if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
+            print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), no spaces, not begin with numbers.")
+        else:
+            return usernameFromInput
+        attempts += 1
+
+    print("\nExhausted all " + str(max_attempts) + " attempts, assigning new username instead ....")
+    return generate_username()[0]
+
 
 def greet_user(name):
     # Greet the user
