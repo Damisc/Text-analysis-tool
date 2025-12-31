4f73644172fd7d24826f9ddb317f576014f09358
Damisc <dammyoladapo2021@gmail.com>
2025-12-31 19:39:56 +0100

    added stopword check to word list filtering

--- analyze.py
+++ analyze.py
@@ -1,13 +1,15 @@
 from random_username.generate import generate_username
+import re
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet, stopwords
 
+nltk.download("stopwords")
 nltk.download("wordnet")
 nltk.download("averaged_perceptron_tagger_eng")
 wordLematizer = WordNetLemmatizer()
-import re
+stopWords = set(stopwords.words("english"))
 
 
 
@@ -104,15 +106,15 @@ def cleanseWordList(posTaggedWordTuples):
         word = posTaggedWordTuple[0]
         pos = posTaggedWordTuple[1]
         cleansedWord = word.replace(".", "").lower()
-        if (not re.search(invalidWordPattern, cleansedWord)) and len(word) > 1:
+        if (not re.search(invalidWordPattern, cleansedWord)) and len(cleansedWord) > 1 and cleansedWord not in stopWords:
             cleansedWords.append(wordLematizer.lemmatize(cleansedWord, treebankPosToWordnetPos(pos)))
     return cleansedWords
 
 
 # Get user details
-welcome_user()
-username = get_username()
-greet_user(username)
+# welcome_user()
+# username = get_username()
+# greet_user(username)
 
 # Extract and tokenize text
 articleTextRaw = getArticleText()
