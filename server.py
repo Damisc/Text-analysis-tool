c3770b2bb121015c4b1f4ed49f82db01f0588049
Damisc <dammyoladapo2021@gmail.com>
2026-01-08 16:09:20 +0100

    added analyze-stock request parameter validation

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # importing flask module in the project is mandatory
 # An object of flask class is our WSGI application
-from flask import Flask
+
+from flask import Flask, abort
 
 # Flask constructor takes the name of current module (__name__) as argument
 app = Flask(__name__)
@@ -13,6 +14,8 @@ def healthCheck():
 
 @app.route("/analyze-stock/<ticker>")
 def analyzeStock(ticker):
+    if len(ticker) > 5 or not ticker.isidentifier():
+        abort(400, "invalid ticker symbol")
     return {"data": "analysis for " + ticker + " coming soon"}
 
 # main driver function
