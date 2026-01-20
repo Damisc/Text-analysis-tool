3f506485cfdfa997c4287c62075cd25f8004b153
Damisc <dammyoladapo2021@gmail.com>
2026-01-20 10:37:28 +0100

    prepared server for render deployment

--- server.py
+++ server.py
@@ -44,4 +44,5 @@ def analyzeTextHandler():
 # main driver function
 if __name__ == "__main__":
     # run() method of Flask class runs the application on the local development server
-    app.run()
+    app.run(host="0.0.0.0")
+    
